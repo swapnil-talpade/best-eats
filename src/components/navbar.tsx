764d3d7bbd174122e048ce91946e17f4ac1d8663
swapnil-talpade <swapniltalpade41@gmail.com>
2023-02-08 17:52:47 +0530

    adds initial setup and navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,35 @@
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { BsFillCartFill } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
+      <div className="flex items-center">
+        <div className="cursor-pointer">
+          <AiOutlineMenu size={30} />
+        </div>
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+          Best <span className="font-bold">Eats</span>
+        </h1>
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-black text-white rounded-full p-2">Delivery</p>
+          <p className="p-2 text-just-black">Pickup</p>
+        </div>
+      </div>
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+        <AiOutlineSearch size={20} className="text-black" />
+        <input
+          type="text"
+          placeholder="Search food"
+          className="bg-transparent p-2 focus:outline-none w-full text-black"
+        />
+      </div>
+      <button className="bg-white text-black hidden md:flex items-center py-2 rounded-full">
+        <BsFillCartFill size={20} className="mr-2" />
+        Cart
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#90ee02",
+        secondary: "#6002ee",
+        "just-black": "#000000",
+        "space-grey": "#131313",
+      },
+    },
+  },
+  plugins: [],
+};
