e9755a4a8649b631d6318126278ef60d21fbf705
swapnil-talpade <swapniltalpade41@gmail.com>
2023-02-09 12:02:27 +0530

    conditionally renders overlay

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   AiFillTag,
   AiOutlineClose,
@@ -11,10 +12,11 @@ import { TbTruckDelivery } from "react-icons/tb";
 import Toggle from "./toggle-theme";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div className="cursor-pointer" onClick={() => setNav(true)}>
           <AiOutlineMenu size={30} className="dark:text-white" />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2 dark:text-white">
@@ -40,41 +42,46 @@ const Navbar = () => {
       <div className="ml-2">
         <Toggle />
       </div>
-      <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
-      <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
-        <AiOutlineClose
-          size={30}
-          className="absolute right-4 top-4 cursor-pointer"
-        />
-        <h2 className="text-2xl p-4">
-          Best <span className="font-bold">Eats</span>
-        </h2>
-        <nav>
-          <ul className="flex flex-col p-4 text-gray-800">
-            <li className="text-xl py-4 flex">
-              <TbTruckDelivery size={25} className="mr-4" /> Orders
-            </li>
-            <li className="text-xl py-4 flex">
-              <MdFavorite size={25} className="mr-4" /> Favorites
-            </li>
-            <li className="text-xl py-4 flex">
-              <FaWallet size={25} className="mr-4" /> Wallet
-            </li>
-            <li className="text-xl py-4 flex">
-              <MdHelp size={25} className="mr-4" /> Help
-            </li>
-            <li className="text-xl py-4 flex">
-              <AiFillTag size={25} className="mr-4" /> Promotions
-            </li>
-            <li className="text-xl py-4 flex">
-              <BsFillSaveFill size={25} className="mr-4" /> Best Ones
-            </li>
-            <li className="text-xl py-4 flex">
-              <FaUserFriends size={25} className="mr-4" /> Invite Friends
-            </li>
-          </ul>
-        </nav>
-      </div>
+      {nav && (
+        <>
+          <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+          <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
+            <AiOutlineClose
+              size={30}
+              className="absolute right-4 top-4 cursor-pointer"
+              onClick={() => setNav(false)}
+            />
+            <h2 className="text-2xl p-4">
+              Best <span className="font-bold">Eats</span>
+            </h2>
+            <nav>
+              <ul className="flex flex-col p-4 text-gray-800">
+                <li className="text-xl py-4 flex">
+                  <TbTruckDelivery size={25} className="mr-4" /> Orders
+                </li>
+                <li className="text-xl py-4 flex">
+                  <MdFavorite size={25} className="mr-4" /> Favorites
+                </li>
+                <li className="text-xl py-4 flex">
+                  <FaWallet size={25} className="mr-4" /> Wallet
+                </li>
+                <li className="text-xl py-4 flex">
+                  <MdHelp size={25} className="mr-4" /> Help
+                </li>
+                <li className="text-xl py-4 flex">
+                  <AiFillTag size={25} className="mr-4" /> Promotions
+                </li>
+                <li className="text-xl py-4 flex">
+                  <BsFillSaveFill size={25} className="mr-4" /> Best Ones
+                </li>
+                <li className="text-xl py-4 flex">
+                  <FaUserFriends size={25} className="mr-4" /> Invite Friends
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </>
+      )}
     </div>
   );
 };
